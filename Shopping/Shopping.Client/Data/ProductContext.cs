47fa44ae556a7c664fbbf7e962dc9e923f23fe85
Jafidan <r.alagezov@gmail.com>
2024-10-31 15:27:32 +0400

    Creating a Project

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,51 @@
+﻿using Shopping.Client.Models;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product()
+            {
+                Name = "IPhone X",
+                Description = "Description",
+                ImageFile = "product-1.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Name = "IPhone X",
+                Description = "Description",
+                ImageFile = "product-2.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Name = "IPhone X",
+                Description = "Description",
+                ImageFile = "product-3.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Name = "IPhone X",
+                Description = "Description",
+                ImageFile = "product-4.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Name = "IPhone X",
+                Description = "Description",
+                ImageFile = "product-5.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+        };
+    }
+}
